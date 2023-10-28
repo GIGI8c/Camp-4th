3cca9e35007a289a06c8e3fcc67f20d0f5a3b55a
利绪 <89396906+Edushe@users.noreply.github.com>
2023-10-28 18:14:00 +0800

    bug修复

--- Assets/Scripts/Enemy/Bee/Enemy_Bee.cs
+++ Assets/Scripts/Enemy/Bee/Enemy_Bee.cs
@@ -4,17 +4,12 @@ using UnityEngine;
 
 public class Enemy_Bee : EnemyGeneral
 {
-    public override void Move()//蜜蜂的移动效果
+    public override void Move()//重写蜜蜂的移动效果
     {
-        if(canMove)
-        rb.velocity = new Vector2(rb.velocity.x, speed * Time.deltaTime);
-    }
+        if (canMove == false) return;
 
-    private void Update()
-    {
-        rb.velocity = new Vector2(rb.velocity.x, speed * Time.deltaTime);
-        if (check.isTouchTop || check.isGround)//接触到天花板或地板时
-            speed=-speed;//进行方向取反
+        if ((check.isGround && speed < 0)||(check.isTouchTop&&speed>0)) speed = -speed;//接触到墙面或天花板时，移动方向取反
+        rb.velocity = new Vector2(rb.velocity.x, speed * Time.deltaTime);//进行移动
     }
 
     public override void Hurt(Transform attacker)//蜜蜂的受伤效果
